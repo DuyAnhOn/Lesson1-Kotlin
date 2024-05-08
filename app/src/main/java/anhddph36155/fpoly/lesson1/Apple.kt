431dbf9817be024e019cc5f35cd54d81343b5135
Admin <anhsg512@gmail.com>
2024-05-08 15:32:10 +0700

    Initial commit

---
+++ app/src/main/java/anhddph36155/fpoly/lesson1/Apple.kt
@@ -0,0 +1,87 @@
+package anhddph36155.fpoly.lesson1
+
+fun main() {
+    //Lesson 1
+//        print("Nguyễn Văn Anh - PS123456\n")
+//        print("=========================\n")
+//        println("Quanh năm buôn bán ở mom sông")
+//        println("Nuôi đủ năm con với một chồng")
+//        println("\tlặn lội thân cờ khi quãng vắng")
+//        println("\teo sèo mặt nước buổi đò đồng")
+//        println("Một duyên hai nợ âu đành phận")
+//        println("Năm nắng mười mưa há chẳng công")
+//        println("\tCha mẹ thói đời \"ăn ở bạc\"")
+//        println("\tCó chồng hờ hững cũng như không")
+
+    //Lesson 2
+    var a = 0.0
+    var b = 0.0
+
+    println("Enter a: ")
+    var s = readLine()
+    if (s != null) a = s.toDouble()
+
+    println("Enter b: ")
+    s = readLine()
+    if (s != null) b = s.toDouble()
+
+    println("$a + $b = ${a.plus(b)}")
+    println("$a - $b = " + (a-b))
+    println("$a * $b = ${a.times(b)}")
+    println("$a  / $b = ${a.div(b)}")
+
+    //Print
+//    print("Hi\n")
+//    println("It's me")
+//    print("\tI'm problem")
+
+    //Varible
+//    var a = 1
+//    var b = 2
+//    b = a
+//    println("a = " + a)
+//    println("b = " + b)
+//    println("$a")
+//
+//    val c: Int
+//    c = 3
+
+    //Array
+//     var array = intArrayOf(1, 2, 3)
+//    println(array[2])
+
+    //Function
+//    fun sum(a: Int, b: Int): Int{
+//        return a+b
+//    }
+
+    //Unary Operator
+//    var a = 1
+//    var b = a.unaryPlus()
+//    var c = a.unaryMinus()
+//    println(b)
+//    println(c)
+
+    //Arithmetic operator
+//    var a = 8.plus(2)
+//    println(a)
+//
+//    var b = 10; var c =6
+//    var d = b.minus(c)
+//    println("$b - $c = $d")
+//
+//    println(2.times(3))
+//
+//    println(10.div(2))
+//
+//    println(10.rem(3))
+
+    //Comparison operator
+//    var a = 1; var b = 2; var c =2
+//    println(a.equals(b))
+//    println(!a.equals(b))
+//    println(b.compareTo(a))
+//    println(c.compareTo(b))
+//    println(a.compareTo(c))
+
+}
